17d8e0ab73618d41350b99eb437174c122b33012
Darch02 <juanajaramillom@hotmail.com>
2024-10-05 17:35:08 -0500

    desarrollo de la landing page

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,99 +1,29 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className="grid grid-cols-2 grid-rows-1 items-center m-12">
+      <div className="grid space-y-10">
+        <h1 className="font-titulos font-bold text-h1 text-center">Generador de tablas nutricionales</h1>
+        <div className="px-24 py-9 bg-gris rounded-xl">
+          <h3 className="font-titulos font-bold text-h3 text-center">Ingresa ingredientes a una base de datos de materias primas </h3>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="px-24 py-9 bg-gris rounded-xl">
+          <h3 className="font-titulos font-bold text-h3 text-center">Genera tablas nutricionales en varios formatos con solo un clic. </h3>
+        </div>
+        <div className="grid grid-cols-2 grid-rows-1 items-center">
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="font-titulos font-bold text-h3 text-center">Registrate para comenzar</h3>
+            <button className=" border font-parrafos text-p text-center text-blanco bg-negro-claro hover:bg-blanco hover:border-b-negro-claro hover:text-negro-claro py-4 px-6 rounded-xl mt-4">Registrarse</button>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="font-titulos font-bold text-h3 text-center">Si ya tienes cuenta, inicia sesion</h3>
+            <button className=" border font-parrafos text-p text-center text-blanco bg-negro-claro hover:bg-blanco hover:border-b-negro-claro hover:text-negro-claro py-4 px-6 rounded-xl mt-4">Iniciar Sesión</button>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <img className=""src="/imagenes/imagen_principal.png" alt="imagen principal"></img>
+      </div>
     </div>
   );
 }
--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    colors:{
+      "gris": "#CECECE",
+      "negro-claro": "#212121",
+      "blanco": "white",
+    },
+    extend: {
+      fontFamily: {
+        'parrafos': ['Roboto', 'sans-serif'],
+        'titulos': ['Montserrat', 'sans-serif'],
+      },
+      fontSize: {
+        'h1': '2rem',  // 32px
+        'h2': '1.5rem',    // 24px
+        'h3': '1.125rem',   // 18px
+        'p': '1rem',  // 16px
+      },
+    },
   },
   plugins: [],
 };
